e4eaa38e9875b1a98e23ba99ecb8293848eff803
wang_etsu <31350618+wangEtsu@users.noreply.github.com>
2021-08-28 18:31:14 +1000

    components init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  // this is the root component of a react app
+  // only this component got rendered as a new html page, thus called "SPA"
+
+  const expenses = [
+    {
+      id: "e1",
+      title: "toilet paper",
+      amount: "10",
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "lunch",
+      amount: "14",
+      date: new Date(2020, 6, 14),
+    },
+    {
+      id: "e3",
+      title: "laptop",
+      amount: "1400",
+      date: new Date(2021, 2, 14),
+    },
+    {
+      id: "e4",
+      title: "coffee",
+      amount: "3",
+      date: new Date(2019, 7, 1),
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* have to use uppercase, which indicates its a custom component, if not its an HTML element */}
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,24 @@
+import "./ExpenseItem.css";
+
+// first component, remember its just a JS function
+function ExpenseItem(props) {
+const month = props.date.toLocaleString('en-US', { month: 'long' });
+const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+const year = props.date.getFullYear();
+
+  return (
+    <div className="expense-item">
+      <div>
+         <div>{month}</div> 
+         <div>{year}</div> 
+         <div>{day}</div> 
+      </div>
+      <div>
+        <h2 className="expense-item__description">{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
