5b5bbe85ea782a09becf4054cdac9985fe05e370
wangetsu1229@gmail.com <wangetsu1229@gmail.com>
2021-08-29 12:49:27 +1000

    composition: add wrapper component

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,8 +1,9 @@
 import ExpenseItem from './ExpenseItem'
+import Card from './Card';
 
 function Expense(props) {
     return (
-        <div>
+        <Card>
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
@@ -18,7 +19,7 @@ function Expense(props) {
                 amount={props.items[2].amount}
                 date={props.items[2].date}
             ></ExpenseItem>
-        </div>
+        </Card>
     )
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from './Card';
 
 // first component, remember its just a JS function
 function ExpenseItem(props) {
 
     return (
-        <div className="expense-item">
-        <ExpenseDate date={props.date}></ExpenseDate>
-        <div>
-            <h2 className="expense-item__description">{props.title}</h2>
-            <div className="expense-item__price">${props.amount}</div>
-        </div>
-        </div>
+        <Card className="expense-item">
+            <ExpenseDate date={props.date}></ExpenseDate>
+            <div>
+                <h2 className="expense-item__description">{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </Card>
     );
 }
 
