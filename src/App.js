493e493dcf89b9d513d08eeb048a5512f7162c87
wangetsu1229@gmail.com <wangetsu1229@gmail.com>
2021-08-30 22:46:54 +1000

    state to update component (re-evaluate)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 import Expense from "./components/Expenses/Expense";
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from '../UI/Card';
@@ -5,13 +7,20 @@ import Card from '../UI/Card';
 // first component, remember its just a JS function
 function ExpenseItem(props) {
 
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle('Updated!');
+    }
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
-            <div>
-                <h2 className="expense-item__description">{props.title}</h2>
+            <div className="expense-item__description">
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change</button>
         </Card>
     );
 }
