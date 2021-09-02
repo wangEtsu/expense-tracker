47af2ff4c4520d67b9dcfe60083f25eeda37765e
wang_etsu <31350618+wangEtsu@users.noreply.github.com>
2021-09-02 22:46:52 +1000

    add new expense

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
+import React, { useState } from "react";
+
 import "./App.css";
 
 import Expense from "./components/Expenses/Expense";
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "toilet paper",
+    amount: "10",
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "lunch",
+    amount: "14",
+    date: new Date(2020, 6, 14),
+  },
+  {
+    id: "e3",
+    title: "laptop",
+    amount: "1400",
+    date: new Date(2021, 2, 14),
+  },
+  {
+    id: "e4",
+    title: "coffee",
+    amount: "3",
+    date: new Date(2019, 7, 1),
+  },
+];
+
 function App() {
   // this is the root component of a react app
   // only this component got rendered as a new html page, thus called "SPA"
 
-  const expenses = [
-    {
-      id: "e1",
-      title: "toilet paper",
-      amount: "10",
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: "e2",
-      title: "lunch",
-      amount: "14",
-      date: new Date(2020, 6, 14),
-    },
-    {
-      id: "e3",
-      title: "laptop",
-      amount: "1400",
-      date: new Date(2021, 2, 14),
-    },
-    {
-      id: "e4",
-      title: "coffee",
-      amount: "3",
-      date: new Date(2019, 7, 1),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  const addExpenseHandler = (expense) =>{
-    console.log("In App.js");
-    console.log(expense);
-  }
+  const addExpenseHandler = expense => {
+    setExpenses(prevExpeenses => {
+      return [expense, ...prevExpeenses]
+    });
+  };
 
   return (
     <div className="App">
