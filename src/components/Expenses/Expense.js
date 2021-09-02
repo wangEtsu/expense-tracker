781fbb31356896535a004c0d1721d3784c5f16bc
wang_etsu <31350618+wangEtsu@users.noreply.github.com>
2021-09-03 00:54:28 +1000

    always add key when rendering list of item

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,26 +1,36 @@
-import React, {useState} from 'react';
-import ExpenseItem from './ExpenseItem';
-import ExpenseFilter from './ExpenseFilter';
-import Card from '../UI/Card';
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
+import Card from "../UI/Card";
 
 function Expense(props) {
-    console.log("new list of items");
-    console.log(props.items);
-    const [filteredYear, setFilteredYears] = useState('2020')
+  console.log("new list of items");
+  console.log(props.items);
+  const [filteredYear, setFilteredYears] = useState("2020");
 
-    // this is triggered by child component: ExpenseFilter.js
-    const filterChangeHandler = (selectedYear) => {
-        setFilteredYears(selectedYear);
-    }
+  // this is triggered by child component: ExpenseFilter.js
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYears(selectedYear);
+  };
 
-    return (
-        <div>
-            <Card className='expenses'>
-                <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {props.items.map(expense => (<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />))}
-            </Card>
-        </div>
-    )
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
 }
 
 export default Expense;
